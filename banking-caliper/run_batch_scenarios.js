f3058114c265a6fe43f278416197472ac3f52b06
d24it143 <d24it143@charusat.edu.in>
2026-07-16 23:59:13 +0530

    feat: add benchmark results for 4500 tx scenarios

--- banking-caliper/run_batch_scenarios.js
+++ banking-caliper/run_batch_scenarios.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
 
-const transactionCounts = [2500, 3000, 3500, 4000, 4500];
+const transactionCounts = [4500];
 
 const cases = [
   {
